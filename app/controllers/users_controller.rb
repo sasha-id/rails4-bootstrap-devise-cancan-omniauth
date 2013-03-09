e9ef2b42f04aa4ac8835bb5191df930bda261248
Alex Yusupov <alex@klepa.co>
2013-03-09 16:30:34 -0500

    Removed json view from users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,10 +23,8 @@ class UsersController < ApplicationController
     respond_to do |format|
       if @user.save
         format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @user }
       else
         format.html { render action: 'new' }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -37,10 +35,8 @@ class UsersController < ApplicationController
 
       if @user.update_attributes(user_params)
         format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { head :no_content }
       else
         format.html { render action: 'edit' }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -51,7 +47,6 @@ class UsersController < ApplicationController
     @user.destroy
     respond_to do |format|
       format.html { redirect_to users_url }
-      format.json { head :no_content }
     end
   end
 
--- app/views/users/show.json.jbuilder
+++
@@ -1,1 +0,0 @@
-json.extract! @user, :created_at, :updated_at
