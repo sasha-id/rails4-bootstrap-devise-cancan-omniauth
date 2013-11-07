c3571d65b8eec588f68d0ef75753c11c159d620c
Conner Fritz <me@connerfritz.com>
2013-11-07 15:50:45 -0500

    Update passwords_controller.rb
    Add reset_password_token to permitted parameters, fixing issue with Devise recoverable.

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,6 +1,6 @@
 class Users::PasswordsController < Devise::PasswordsController
   def resource_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    params.require(:user).permit(:email, :password, :password_confirmation, :reset_password_token)
   end
   private :resource_params
 end
