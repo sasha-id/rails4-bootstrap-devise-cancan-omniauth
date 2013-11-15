736219c9a978111a95191ca980eebc230587b174
Emil Sågfors <emil.sagfors@iki.fi>
2013-11-15 19:43:28 +0200

    Include the haml-rails gem

--- rails4-bootstrap.rb
+++ rails4-bootstrap.rb
@@ -29,6 +29,7 @@ add_source 'https://rubygems.org'
 gem 'rails', '4.0.0'
 gem "mongoid", github: "mongoid/mongoid"
 
+gem 'haml-rails', '~> 0.4'
 gem 'sass-rails', '~> 4.0.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.0.0'
