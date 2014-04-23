d9a75a495deee585ebfbf9587d8127c58443cc22
Sreejesh K P <sreejesh@sreejesh-Vostro-230.(none)>
2014-04-23 17:24:51 +0530

    fix for registration - Issue #15

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,10 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+  before_filter :resource_params, if: :devise_controller?
+
   def resource_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password)
+    devise_parameter_sanitizer.for(:sign_up) {|user| user.permit(:first_name, :last_name, :email, :password, :password_confirmation)}
+    devise_parameter_sanitizer.for(:account_update) {|user| user.permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password)}
   end
+  
   private :resource_params
 end
